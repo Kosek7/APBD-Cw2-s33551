24d2aad4d564b4180fba5e0cdeddacf8dcefa19c
Dawid-Piotrowski-s33551 <s33551@pjwstk.edu.pl>
2026-03-24 23:30:54 +0100

    Add services

---
+++ Services/UserService.cs
@@ -0,0 +1,21 @@
+class UserService
+{
+    private readonly IUserStorage userStorage;
+    private readonly IRentralStorage rentalStorage;
+
+    public UserService(IUserStorage userStorage, IRentalStorage rentalStorage)
+    {
+        this.userStorage = userStorage;
+        this.rentalStorage = rentalStorage;
+    }
+
+    public void AddUser(User user) => userStorage.Add(user);
+
+    public int GetActiveRentalsCount(User user) => rentalStorage.GetByUser(user).Count(r => r.ReturnDate == null);
+
+    public bool CanRent(User user)
+    {
+        int limit = user is Student ? 2 : 5;
+        return GetActiveRentalsCount(user) < limit;
+    }
+}
